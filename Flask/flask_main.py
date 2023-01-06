aa7b0dfff6c59b09f138e38595b558b71247f551
Sa2a <34524576+Sa2a@users.noreply.github.com>
2023-01-06 17:51:22 +0200

    flask ready

--- Flask/flask_main.py
+++ Flask/flask_main.py
@@ -6,10 +6,25 @@ import sys, os
 current_directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(current_directory))
 # import Common
+import numpy as np
 from Scanner import scan_text
-app= Flask(__name__,template_folder=current_directory)
+app= Flask(__name__,template_folder="./grad website/startbootstrap-agency-gh-pages",static_folder="./grad website/startbootstrap-agency-gh-pages")
 
-
+originalData = []
+# originalData = []
+def applyFilter(novelTh,meaningTh):
+    global originalData
+    data  = originalData.copy()
+    conditions = [
+        (data['Correct'] == True) & (data["Relevance before correction"] <= meaningTh),
+        (data['Correct'] == True) & (data["Relevance before correction"] > meaningTh),
+        (data['Correct'] != True) & (data[["Relevance before correction","Relevance after correction"]].max(axis=1) <= novelTh),
+        (data['Correct'] != True) & (data[["Relevance before correction","Relevance after correction"]].max(axis=1) > novelTh)
+        ]
+    values = ['Meaning Change', 'Meaning No Change', 'Novel', 'Misspelled']
+    # create a new column and use np.select to assign values to it using our lists as arguments
+    data['Label'] = np.select(conditions, values)
+    return data
 
 
 
@@ -20,8 +35,24 @@ def homepage():
 @app.route('/predict', methods = ['POST','GET'])
 def predict():
     text=request.form['texta']
-    data= scan_text(text)
-    return render_template(index_file,**locals(),   tables=[data.to_html(classes='data')], titles=data.columns.values)
-        
+    global originalData
+    originalData= scan_text(text)
+    return render_template(index_file,**locals(),   tables_original=[originalData.to_html(classes='data')], titles=originalData.columns.values)
+
+@app.route('/predict/filter', methods = ['POST','GET'])
+def filter():
+    novelThreshold=np.float64(request.form['novel_threshold'])
+    meaningThreshold=np.float64(request.form['meaning_threshold'])
+    print(type(meaningThreshold))
+    data  = applyFilter(novelThreshold,meaningThreshold)
+    return render_template(index_file,**locals(),   
+    tables_filter=[
+        data.query('Label ==  "Meaning No Change"').to_html(classes='data'),
+        data.query('Label == "Meaning Change"').to_html(classes='data'),
+        data.query('Label == "Novel"').to_html(classes='data'),
+        data.query('Label ==  "Misspelled"').to_html(classes='data')
+        # data[(data.Label=='Novel') or (data.Label == 'Misspelled')].to_html(classes='data'),
+    ],names =["Meaning No Change","Meaning Change","Novel","Misspelled"], titles=data.columns.values)
+
 if __name__ == "__main__":
     app.run(debug=True,port=8000, host= "0.0.0.0")
